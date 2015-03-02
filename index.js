e0acb5a0c86bd972d9e954620be316fca424f71c
Michael Gilley <michaelgilley@gmail.com>
2015-03-02 12:36:25 -0800

    Init commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+var express = require('express')
+  , morgan = require('morgan')
+  , app = express()
+  , quotes = require('./data').quotes
+
+app.set('views', __dirname + '/views')
+
+if (app.get('env') === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(express.static('./public'))
+
+app.get('/', function(req, res) {
+  var pick = Math.floor(Math.random() * quotes.length - 1)
+  res.render('index.jade', {quote: quotes[pick].text})
+})
+
+app.listen(process.env.PORT || 4000)
